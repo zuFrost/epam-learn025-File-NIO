b5ada5e81719a397cbea859bcf1ad9851b51f1ae
Alex Frost <zufrost.a@gmail.com>
2020-04-24 17:29:46 +0300

    update ParseData class

--- scr/com/zufrost/learn/ParseData.java
+++ scr/com/zufrost/learn/ParseData.java
@@ -11,16 +11,9 @@ public class ParseData {
          * @param localDate дата
          * @return строка с форматом день-месяц-год(01-01-1970)
          */
-        /*@Override
-        public String parseDate(LocalDate localDate) {
-            return null;
-        }*/
         LocalDate localDate = LocalDate.now();
-//        System.out.println(localDate.toString());
-
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        String dateString = localDate.format(formatter);
-        System.out.println(dateString);
+        System.out.println(localDate.format(formatter));
 
     }
 }
