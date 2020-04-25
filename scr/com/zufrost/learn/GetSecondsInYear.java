8967f4c997cfc67622f8bdcd581fc4f15341d49a
Alex Frost <zufrost.a@gmail.com>
2020-04-25 06:19:12 +0300

    make GetSecondsInYear class

--- scr/com/zufrost/learn/GetSecondsInYear.java
+++ scr/com/zufrost/learn/GetSecondsInYear.java
@@ -1,5 +1,10 @@
 package com.zufrost.learn;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.time.ZoneOffset;
+
 public class GetSecondsInYear {
     public static void main(String[] args) { //TODO
         /**
@@ -7,10 +12,15 @@ public class GetSecondsInYear {
          *
          * @return число секунд
          */
-        /*@Override
-        public long getSecondsInYear(int year) {
-            return 0;
-        }*/
-        int year = 2011;
+        int year = 2020;
+        Year yearFrom = Year.of(year);
+        System.out.println("считаем количество секунд в промежутке между " + yearFrom + " годом и " + yearFrom.plusYears(1) + " годом");
+        LocalDateTime startTime = LocalDateTime.of(year, 1, 1, 0, 0, 0);
+        LocalDateTime finishTime = startTime.plusYears(1);
+//        LocalDateTime finishTime = LocalDateTime.of(year, 12, 31, 23, 59, 59);
+        long secondsFromEpochToStart = finishTime.toEpochSecond(ZoneOffset.of("Z")) - startTime.toEpochSecond(ZoneOffset.of("Z"));
+        System.out.println(secondsFromEpochToStart);
+
+
     }
 }
