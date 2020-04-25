672230d974ac3c5f49692e343186cc557ff83ab0
Alex Frost <zufrost.a@gmail.com>
2020-04-25 05:48:58 +0300

    create but not complite GetSecondsInYear Class

---
+++ scr/com/zufrost/learn/GetSecondsInYear.java
@@ -0,0 +1,16 @@
+package com.zufrost.learn;
+
+public class GetSecondsInYear {
+    public static void main(String[] args) { //TODO
+        /**
+         * Метод считает число секунд в заданном году
+         *
+         * @return число секунд
+         */
+        /*@Override
+        public long getSecondsInYear(int year) {
+            return 0;
+        }*/
+        int year = 2011;
+    }
+}
